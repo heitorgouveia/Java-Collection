bb4567292453eb97a5e0eb768ce8e79441e8c947
Heitor G. Nunes <heitorgouveianunes@gmail.com>
2024-09-13 15:00:50 -0300

    Ordenação Java Collections

---
+++ src/ordenacao/ordenacaoPessoas/OrdenacaoPessoas.java
@@ -0,0 +1,34 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class OrdenacaoPessoas{
+
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
+    }
+    
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoaPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorIdade);
+        return pessoaPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoaPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorAltura, new ComparatorPorAltura());
+        return pessoaPorAltura;
+    }
+
+
+    
+}
+
